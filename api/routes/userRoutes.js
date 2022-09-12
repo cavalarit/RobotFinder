44321512d3543880929c0b1eb60f71107035ebb6
cavalarit <thiago.cavalari@gmail.com>
2022-09-12 08:42:17 +0100

    Delete api/routes directory

--- api/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var userController = require('../controllers/userController');
-
-module.exports = function(app) {
-  app.route('/getUsers')
-    .get(userController.getUserList);
-};
