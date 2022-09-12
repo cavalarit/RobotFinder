cc69957006fe901e75f1597850e03f2a0b2f923b
cavalarit <thiago.cavalari@gmail.com>
2022-09-12 08:42:46 +0100

    Update loginController.js

--- api/controllers/loginController.js
+++ api/controllers/loginController.js
@@ -1,12 +1,13 @@
-'use strict';
+require('../models/contants');
 
-exports.login = function(req, res) {
-  if (USER_USER == req.body.user && USER_PASSWORD == req.body.password) {
-    res.status(200);
-    res.json({'token':'5DE6E0C53BB8BB6CBB392526FA278D47'});
-
-  }else{
-    res.status(401);
-    res.json({'error':'invalid user and password'});
-  }
+const login = (user, password) => {
+  return new Promise((resolve, reject) => {
+    if (USER_USER == user && USER_PASSWORD == password) {
+      resolve(JSON.stringify({'token':'5DE6E0C53BB8BB6CBB392526FA278D47'}));
+    }else{
+      reject(JSON.stringify({'error':'invalid user and password'}));
+    }
+  });
 };
+
+exports.login = login;
